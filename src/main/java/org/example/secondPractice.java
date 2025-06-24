1728b22ce43974e2b91c1f49abef9013b7f4da7e
Iulia Sandu <isandu@griddynamics.com>
2025-06-24 10:50:48 +0300

    String examples classes

--- src/main/java/org/example/secondPractice.java
+++ src/main/java/org/example/secondPractice.java
@@ -2,13 +2,12 @@ package org.example;
 
 import java.lang.*;
 import java.nio.CharBuffer;
+import java.util.StringJoiner;
+import java.util.StringTokenizer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import static org.example.TextProcessor.containsDigit;
 
-
-// Compute distance light travels using long variables.
 class Dog {
     String breed;
     String name;
@@ -39,7 +38,7 @@ public class secondPractice { // Имя файла должно совпадат
         Dog myDog = new Dog("Golden Retriever", "Buddy"); // Обычный синтаксис конструктора
         myDog.bark();
 
-        /*/ Declare String without using new operator - String literal
+        /* Declare String without using new operator - String literal
         String s = "GeeksforGeeks";
 
         // Prints the String
@@ -66,8 +65,37 @@ public class secondPractice { // Имя файла должно совпадат
         char[] charArray = {'H', 'e', 'l', 'l', 'o'};
         CharBuffer wrappedBuffer = CharBuffer.wrap(charArray);
         System.out.println(wrappedBuffer); // Hello (toString() выводит содержимое)
+        System.out.println("-------------------------------------\n");
 
         StringBuilder str = new StringBuilder();
         str.append("GFG");
+
+        String text = "Hello World Java";
+        StringTokenizer tokenizer = new StringTokenizer(text);
+
+        String text2 = "apple,banana;orange.grape";
+        StringTokenizer tokenizer2 = new StringTokenizer(text, ",;.");
+        // Разделители: ',', ';', '.'
+        // Токены: "apple", "banana", "orange", "grape"
+
+        String text3 = "hello;world";
+        StringTokenizer tokenizer3 = new StringTokenizer(text, ";", true);
+        // Токены: "hello", ";", "world"
+
+        while(tokenizer.hasMoreTokens()) {
+            System.out.println(tokenizer.nextToken());
+        }
+        System.out.println("-------------------------------------\n");
+
+        StringJoiner sj1 = new StringJoiner(", "); // Разделитель: ", "
+        sj1.add("apple");
+        sj1.add("banana");
+        System.out.println(sj1.toString()); // apple, banana
+        System.out.println("-------------------------------------\n");
+
+        StringJoiner sj2 = new StringJoiner(":", "{", "}"); // Разделитель: ":", Префикс: "{", Суффикс: "}"
+        // sj2.add("key1");
+        // sj2.add("value1");
+        // System.out.println(sj2.toString()); // {key1:value1}
     }
 }
