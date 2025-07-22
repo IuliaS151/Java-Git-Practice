9478fd1194d52e91fd2d193eb949a77f1c117e30
Iulia Sandu <isandu@griddynamics.com>
2025-07-22 14:14:29 +0300

    Add task5(update) and task6(solve)

--- src/main/java/org/example/homework/task5/BeatlesException.java
+++ src/main/java/org/example/homework/task5/BeatlesException.java
@@ -1,6 +1,8 @@
 package org.example.homework.task5;
+// 3. Define a custom exception : BeatlesException extending Exception, add a ""Beatles"" to a caught exception message
+public class BeatlesException extends Exception{
 
-public class BeatlesException extends  Exception{
+    public BeatlesException() { System.out.println("Beatles ");}
 
     public BeatlesException(String message) {
         super("Beatles " + message);
